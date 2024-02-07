4187ce595fb6b2ca13d7ccaf3b9dfa7df731676c
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-02-07 23:43:43 +0600

    initialize store and slice

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import "./App.css";
-
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div className="h-screen w-full flex justify-center items-center">
+      <div className="flex border border-purple-300 rounded-md bg-slate-50 p-10">
+        <button className="px-3 py-2 rounded-md bg-green-500 m-10 text-xl font-semibold text-white">
+          incriment
+        </button>
+        <h1 className="text-3xl m-10">0</h1>
+        <button className="px-3 py-2 rounded-md bg-red-500 text-xl m-10 font-semibold text-white">
+          decriment
+        </button>
+      </div>
+    </div>
   );
 }
 
